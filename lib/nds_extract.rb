c29ba18819d1dae1dacb8eb09050fc80aeb6afac
Sophia Lyu <shunshunlyu@protonmail.com>
2020-02-05 00:51:16 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -4,9 +4,14 @@ require 'pp'
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
-  pp director_data[:movies]
-  row_length = 0 
-  
+  shortcut = director_data[:movies]
+  row_index = 0 
+  total = 0
+  while row_index < shortcut.length do
+    total += shortcut[row_index][:worldwide_gross]
+    row_index += 1 
+  end
+  total
 end
 
 # Write a method that, given an NDS creates a new Hash
