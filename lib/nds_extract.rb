b11e7702957fb62a2e47d72f84dfa81b8cc6c209
Sophia Lyu <shunshunlyu@protonmail.com>
2020-02-05 00:46:03 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -4,6 +4,7 @@ require 'pp'
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
+  row_index = 0
   while row_index < director_data.length do
     grand_total = 0
     column_index = 0 
