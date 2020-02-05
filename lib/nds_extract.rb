571da2cceef948bc9d54ec48043c8285a77f4144
Sophia Lyu <shunshunlyu@protonmail.com>
2020-02-05 00:55:57 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -20,5 +20,8 @@ end
 # { directorOne => allTheMoneyTheyMade, ... }
 def directors_totals(nds)
   result = {}
-  
+  row_index = 0 
+  while row_index < nds.length do 
+    result[nds[row_index][:name]] = gross_for_director(nds[row_index])
+  end
 end
