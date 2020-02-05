c2a78924d60d7f4d8fe5d670f46257a39d07d818
Sophia Lyu <shunshunlyu@protonmail.com>
2020-02-05 00:44:50 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -4,11 +4,11 @@ require 'pp'
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
-  while row_index < nds.length do
+  while row_index < director_data.length do
     grand_total = 0
     column_index = 0 
-    while column_index < nds[row_index][:movies].length do
-      grand_total += nds[row_index][:movies][column_index][:worldwide_gross]
+    while column_index < director_data[row_index][:movies].length do
+      grand_total += director_data[row_index][:movies][column_index][:worldwide_gross]
       column_index += 1 
     end
   grand_total
